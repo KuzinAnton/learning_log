95c7818d607628e2c37dd4d3033618056e05cb53
KuzinAnton <anton_kuzin@mail.ru>
2020-02-06 12:31:50 +0300

    Регистрация Entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -12,13 +12,13 @@ class Topic(models.Model):
 
 class Entry(models.Model):
 	"""Информация, изученная пользователем по теме"""
-	topic = models.ForeignKey(Topic)
+	topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
 	text = models.TextField()
 	date_added = models.DateTimeField(auto_now_add=True)
 	
 	class Meta:
 		verbose_name_plural = 'entries'
-		
+	
 	def __str__(self):
 		"""Возвращает строковое представление модели"""
 		return self.text[:50] + "..."
