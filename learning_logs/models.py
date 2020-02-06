0e7a0b3d9a9c0ac66f6781dbbf16afe25baf3e87
KuzinAnton <anton_kuzin@mail.ru>
2020-02-06 11:45:15 +0300

    Активизация модели

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Topic(models.Model):
 	"""Тема, уоторую изучает пользователь"""
 	text = models.CharField(max_length=200)
-	date_added = models.DateTimeFieldauto_now_add=True)
+	date_added = models.DateTimeField(auto_now_add=True)
 	
 	def __str__(self):
 		"""Возвращает строковое представление модели"""
